3c2e32e9133db56da0d24be6aeffa8f9d7122731
Leonardo Eloy <leonardo.eloy@gmail.com>
2013-06-24 14:50:50 -0300

    Plugin lock and loaded

--- test/plugin_test.rb
+++ test/plugin_test.rb
@@ -5,11 +5,28 @@ require 'test_helper'
 class PluginTest < Test::Unit::TestCase
 
   context "plugin" do 
-    should "have an author" do
-      plugin = Unitopus::Plugin.new
-
-      assert_respond_to plugin, :author
+    setup do
+      @plugin = Unitopus::Plugin.new
     end
 
+    should "have an author" do
+      assert_respond_to @plugin, :author
+    end
+
+    should "have a description" do
+      assert_respond_to @plugin, :description
+    end    
+
+    should "have a name" do
+      assert_respond_to @plugin, :name
+    end    
+
+    should "have a version" do
+      assert_not_nil Unitopus::Plugin::VERSION
+    end  
+
+    should "handle processing" do
+      assert_respond_to @plugin, :handle
+    end
   end
 end
