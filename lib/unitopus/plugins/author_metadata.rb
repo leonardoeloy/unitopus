4a0ce83811bc20899b19e85604ba21b4e723a9a7
Leonardo Eloy <leonardo.eloy@gmail.com>
2013-06-24 15:16:35 -0300

    Author Metadata plugin

--- lib/unitopus/plugins/author_metadata.rb
+++ lib/unitopus/plugins/author_metadata.rb
@@ -1,13 +1,15 @@
 # encoding: utf-8
 
 module Unitopus
-  class AuthorMetadata < Unitopus::RegexPlugin
-    def initialize
-      @@author = "Leonardo Eloy <leonardo.eloy@gmail.com>"
-      @@name = "Author Metadata"
-      @@description = "Adds a 'published by' in a blog post"
-      @@regex = /^Author:\ (.*)$/
-      @@parameter = "author"
+  module Plugins
+    class AuthorMetadata < Unitopus::RegexPlugin
+      def initialize
+        @author = "Leonardo Eloy <leonardo.eloy@gmail.com>"
+        @name = "Author Metadata"
+        @description = "Adds a 'published by' in a blog post"
+        @regex = /^Author:\ (.*)$/
+        @parameter = :author
+      end
     end
   end
 end  
